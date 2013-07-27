8ff43e344d1f4194603ec29032d49384545de3b9
tybenz <tabenziger@gmail.com>
2013-07-27 10:51:15 -0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,12 @@
+data = {
+  links: [
+    { url: 'www.biausa.org', desc: 'The Brain Injury Association of America' },
+    { url: 'www.ninds.nih.gov/disorders/tbi/detail_tbi.htm', desc: 'National Institute of Neurological Disorders and Stroke' },
+    { url: 'www.tbiguide.com', desc: 'A "Survival Guide" by Dr. Glen Johnson, Clinical Neuropsycologist' },
+    { url: 'www.brainline.org', desc: 'Many resources for preventing, living with, and reating TBI' },
+    { url: 'kcrlegal.com/brain-injury-resource-center.aspx', desc: 'TBI Resource Center presented by Kershaw, Cutter, and Ratinoff LLP, Civil Justice Attorneys' },
+    { url: 'www.tbi-blog.com', desc: 'Blog presented by the Rosenthal Law Firm of Sacramento' },
+    { url: 'www.braininjury.com/recovery.html', desc: 'Medical and legal information about TBI' },
+    { url: 'www.div40.org/pdf/NeuropsychBroch2.pdf', desc: 'An educational pamphlet by the Neuropsychology Division of American Psychological Association' }
+  ]
+};
